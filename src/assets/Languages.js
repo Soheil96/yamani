72eac6ecd4b07283b407ebde62ad5245f834db33
Soheil Sheybani <soheil@datachef.co>
2025-01-05 08:16:09 +0330

    adding multiple languages

---
+++ src/assets/Languages.js
@@ -0,0 +1,6 @@
+export const languages = [
+  { code: 'en', path: '',label: '🇬🇧 English' },
+  { code: 'ar', path: 'ar/', label: '🇸🇦 Arabic' },
+  { code: 'fr', path: 'fr/', label: '🇫🇷 Français' },
+  { code: 'es', path: 'es/', label: '🇪🇸 Español' },
+];
