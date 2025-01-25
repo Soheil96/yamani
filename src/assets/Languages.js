fd41fa9f86d14fbadc44e4e00dbdeca5637ad4d6
Soheil Sheybani <soheil@datachef.co>
2025-01-25 15:23:40 +0330

    refactor menubar

--- src/assets/Languages.js
+++ src/assets/Languages.js
@@ -1,6 +1,6 @@
 export const languages = [
   { code: 'en', path: '',label: '🇬🇧 English' },
-  { code: 'ar', path: 'ar/', label: '🇸🇦 Arabic' },
+  { code: 'ar', path: 'ar/', label: '🇸🇦 العربية' },
   { code: 'fr', path: 'fr/', label: '🇫🇷 Français' },
   { code: 'es', path: 'es/', label: '🇪🇸 Español' },
 ];
